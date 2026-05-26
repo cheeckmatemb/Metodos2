f1f87d9250ee9d353dfb30eeb875b40b8914a305
CheckMate <chris.hdez.mera@gmail.com>
2026-05-25 20:24:53 -0600

    Agregar archivos de proyecto.

---
+++ Metodos2/Metodos2.cpp
@@ -0,0 +1,12 @@
+﻿// Metodos2.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "Metodos2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
